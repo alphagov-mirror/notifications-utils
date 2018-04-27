8fb27851072643a3f0b1b4864075b2b4a8f01481
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2018-04-27 14:33:03 +0100

    allow nicer imports of clients
    previously:
    `from notifications_utils.clients.zendesk.zendesk_client import ZendeskClient`
    
    now:
    `from notifications_utils.clients import ZendeskClient`.

---
+++ notifications_utils/clients/__init__.py
@@ -0,0 +1,3 @@
+from notifications_utils.clients.zendesk.zendesk_client import ZendeskClient  # noqa
+from notifications_utils.clients.redis.redis_client import RedisClient  # noqa
+from notifications_utils.clients.statsd.statsd_client import StatsdClient  # noqa
